3e158dbe14c1b10ee03a5b32c29ae0fb4a28421d
najeeb hall <naj404@hotmail.com>
2017-11-02 01:29:23 +0000

    final

--- request.php
+++ request.php
@@ -11,5 +11,20 @@ $definition = [
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+
+$xmlData= '<?xml version="1.0" encoding="UTF-8"?>
+    <entries>
+        <definition name="definition" author="John">a statement of the exact meaning of a word, especially in a dictionary.</definition>
+        <definition name="bar" author="mary">a place that sells alcholic beverages</definition>
+        <definition name="ajax" author="Kimberly">technique which involves the use of javascript and xml</definition>
+    </entries>';
+    
+if($all){
+    header('Content-Type: text/xml');
+    $xmlOutput = new SimpleXMLElement($xmlData);
+    echo $xmlOutput->asXML();
+}
+else{
+    print "<h3>" . strtoupper($query) . "</h3>";
+    print "<p>" . $definition[$query] . "</p>";
+}
